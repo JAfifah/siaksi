8d52736f1d0d4a2cdb0492cabe486fadd91e4478
JAfifah <jamiatul.afifah2735@gmail.com>
2025-05-06 22:27:08 +0700

    make more neat

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,6 +1,4 @@
 <!-- Main Footer -->
 <footer class="main-footer">
-    <strong>Copyright &copy; 2024 <a href="https://adminlte.io">AdminLTE.io</a>, Boilerplate by <a
-            href="https://github.com/hafizcode02">Hafiz Caniago</a>. </strong>
-    All rights reserved.
+    <strong>Kelompok 3 <a href="https://github.com/JAfifah/siaksi">SIAKSI</a></strong>
 </footer>
--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,10 +1,10 @@
     <!-- Main Sidebar Container -->
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <aside class="main-sidebar sidebar-dark-navy elevation-4">
         <!-- Brand Logo -->
         <a href="index3.html" class="brand-link">
-            <img src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
-                class="brand-image img-circle elevation-3" style="opacity: .8">
-            <span class="brand-text font-weight-light">Laravel Boilerplate</span>
+            <img src="{{ asset('dist/img/JTI.png') }}" alt="AdminLTE Logo"
+                class="brand-image" style="opacity: .8">
+                <span class="brand-text fw-bold fs-4">SIAKSI</span>
         </a>
 
         <!-- Sidebar -->
@@ -46,25 +46,50 @@
                             'childs' => [],
                         ],
                         (object) [
-                            'icon' => 'fas fa-user',
-                            'name' => 'Pengguna',
-                            'link' => '/manajemen-akun',
-                            'childs' => [],
-                            'is_superuser' => true, // Menambahkan field ini untuk mengontrol akses
-                        ],
-                        (object) [
-                            'title' => 'DROPDOWN EXAMPLE',
+                            'title' => 'KRITERIA',
                         ],
                         (object) [
                             'icon' => 'fas fa-book',
-                            'name' => 'Dropdown Example',
+                            'name' => 'Kriteria',
                             'childs' => [
                                 (object) [
-                                    'name' => 'Dropdown 1',
+                                    'name' => 'Kriteria 1',
                                     'link' => '#',
                                 ],
                                 (object) [
-                                    'name' => 'Dropdown 2',
+                                    'name' => 'Kriteria 2',
+                                    'link' => '#',
+                                ],
+                                (object) [
+                                    'name' => 'Kriteria 3',
+                                    'link' => '#',
+                                ],
+                                (object) [
+                                    'name' => 'Kriteria 4',
+                                    'link' => '#',
+                                ],
+                                (object) [
+                                    'name' => 'Kriteria 5',
+                                    'link' => '#',
+                                ],
+                                (object) [
+                                    'name' => 'Kriteria 6',
+                                    'link' => '#',
+                                ],
+                                (object) [
+                                    'name' => 'Kriteria 7',
+                                    'link' => '#',
+                                ],
+                                (object) [
+                                    'name' => 'Kriteria 8',
+                                    'link' => '#',
+                                ],
+                                (object) [
+                                    'name' => 'Kriteria 9',
+                                    'link' => '#',
+                                ],
+                                (object) [
+                                    'name' => 'Suplemen',
                                     'link' => '#',
                                 ],
                             ],
