37c55e013ed2e05aab3bf4c52ad16c5a186ed39f
Ahmad Franklyn Bima Aquilla <franklynbimaa@gmail.com>
2025-05-15 07:45:22 +0700

    kriteria kedua bagian pelaksanaan

--- database/seeders/KriteriaSeeder.php
+++ database/seeders/KriteriaSeeder.php
@@ -104,6 +104,372 @@ class KriteriaSeeder extends Seeder
                 'tahap' => 'penetapan',
                 'nomor' => 2
             ],
+            [
+                'nama' => 'Permendikbud No. 3 Tahun 2020',
+                'tahap' => 'penetapan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Renstra Polinema 2020-2024',
+                'tahap' => 'penetapan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'SK Direktur No. 310 tahun 2019',
+                'tahap' => 'penetapan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Permenristekdikti No. 20 Tahun 2019.',
+                'tahap' => 'penetapan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'SK Direktur No. 620 Tahun 2019',
+                'tahap' => 'penetapan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Perjanjian Kinerja Direktur',
+                'tahap' => 'penetapan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'SOP Jurusan Teknologi Informasi',
+                'tahap' => 'penetapan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Pendirian Polinema OTK - Kemendiknas 147/0/2004',
+                'tahap' => 'penetapan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'OTK Polinema 2023',
+                'tahap' => 'penetapan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'UU ASN',
+                'tahap' => 'penetapan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Pedoman BLU',
+                'tahap' => 'penetapan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'SK Pengelola Jurusan',
+                'tahap' => 'penetapan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Pedoman Kerja sama Kemendikbud',
+                'tahap' => 'penetapan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Pedoman Layanan Kerja sama Polinema',
+                'tahap' => 'penetapan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Sisidiknas UU No 20 Tahun 2023',
+                'tahap' => 'penetapan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'SK GPM',
+                'tahap' => 'penetapan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'SK GKM',
+                'tahap' => 'penetapan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'SK KJM',
+                'tahap' => 'penetapan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'SK P2MPP',
+                'tahap' => 'penetapan',
+                'nomor' => 2
+            ],
+
+            [
+                'nama' => 'SK FGD',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'SK Kurikulum SIB',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Sertifikasi Kompetensi DTPR',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Workshop PBM',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'AMI 2019 - 2023',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Laporan SAI Global',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'OTK Polinema',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Kode etik kemendikbud',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'LAKIN',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Statuta Polinema',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Yudisium Tengah Semester',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Yudisium Akhir Semester',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Notulen RTM - Rapat Tinjauan Manajemen',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Akreditasi ASIIC',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Polinema Paten Tertinggi',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Polinema Peringkat 1 Perguruan Tinggi Vokasi Berbasis Kinerja PkM',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Polinema Peringkat 2 Perguruan Tinggi Vokasi Berbasis Kinerja Penelitian',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'WidyaPadhi',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Polinema 2019 - Kinerja Kemahasiswaan Terbaik',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Kinerja BLU',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Kinerja Kemahasiswaan',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Satker BLu KPPN',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Kinerja Anggaran 2023',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Apresiasi APTV',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Sosial Media Terbaik',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Polinema 2020 - Peminat terbanyak',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Pendampingan Politeknik Negeri Ujung Pandang',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Narasumber persiapan BLu Politeknik Negeri Jakarta',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Tuan Rumah KMIPN',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'MoU Bebras',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Biro Bebras',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Google Pandai',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Bebras Indonesia Challenge',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'IISMA',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'MSIB',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Magang Kewirausahaan',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Hibah PPPTV',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Hibah CF PSTBI',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Struktur Organisasi Polinema',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Rencana Kerja',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'RPS',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Proposal Kegiatan',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Studi Lanjut DTPR',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Daftar Penelitian DTPR',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Daftar PkM DTPR',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Pelatihan DTPR',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Seminar DTPR',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Activity Control',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Kelengkapan dokumen PBM',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Ruang LAB',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Kelengkapan Gudang',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Buku Ajar DTPR',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'Sertifikat ISO 9001:2015',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
+            [
+                'nama' => 'DTPR Auditor AMI',
+                'tahap' => 'pelaksanaan',
+                'nomor' => 2
+            ],
         ];
 
         foreach ($kriteria as $k) {
